3c9a028c2024787bec82d18609463f957a83a59f
himanshuv174 <91723013+himanshuv174@users.noreply.github.com>
2026-06-13 09:58:11 +0530

    first code

---
+++ DSA/Data Stucture/Create_Binary_Tree_from_2D_Array.java
@@ -0,0 +1,63 @@
+
+  Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMapInteger ,TreeNode  d  = new HashMap();
+        HashMapInteger, Integer root = new HashMap();
+
+        for(int[] i descriptions){
+            
+            int parent = i[0];
+            int child = i[1];
+            int isLeft = i[2];
+
+            if(!d.containsKey(parent)){
+                d.put(parent, new TreeNode(parent));
+            }
+
+            if(!d.containsKey(child)){
+                d.put(child, new TreeNode(child));
+            }
+
+
+            if(isLeft == 1){
+                d.get(parent).left = d.get(child);
+            }
+            if(isLeft == 0){
+                d.get(parent).right = d.get(child);
+            }
+
+            if(root.getOrDefault(parent,0)!= -1){
+                root.put(parent,1);
+            }
+
+            root.put(child,-1);
+
+        }
+
+        int rootVal = 0;
+
+        for (var entry  root.entrySet()){
+            if(entry.getValue()==1){
+                rootVal = entry.getKey();
+                break;
+            }
+        }
+
+        return d.get(rootVal);
+
+    }
+}
