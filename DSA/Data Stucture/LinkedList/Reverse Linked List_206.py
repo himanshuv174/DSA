3b5ea83b801d0bfc89db549970248191da63435f
himanshuv174 <91723013+himanshuv174@users.noreply.github.com>
2026-08-28 23:08:38 +0530

    Reverse a Linked List

---
+++ DSA/Data Stucture/LinkedList/Reverse Linked List_206.py
@@ -0,0 +1,47 @@
+# 206. Reverse Linked List
+
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+# Example 2:
+
+
+# Input: head = [1,2]
+# Output: [2,1]
+# Example 3:
+
+# Input: head = []
+# Output: []
+ 
+
+# Constraints:
+
+# The number of nodes in the list is the range [0, 5000].
+# -5000 <= Node.val <= 5000
+ 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None  # Initialize prev as None
+        curr = head  # Start with curr at the head of the list
+
+        while curr:
+            temp = curr.next  # Store the next node
+            curr.next = prev  # Reverse the pointer
+            prev = curr       # Move prev forward
+            curr = temp      # Move curr forward
+
+        return prev  # prev is the new head of the reversed list
+        
